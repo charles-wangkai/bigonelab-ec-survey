a61db8bacab7ec95fe357f5aa277b4de83bccc51
Kai Wang <charles.wangkai@gmail.com>
2019-07-03 14:43:02 +0800

    Add one sample question

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
+import * as Survey from 'survey-react';
+import 'survey-react/survey.css';
+import logo from './logo.png';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header" />
-    </div>
-  );
+class App extends Component {
+  json = {
+    elements: [
+      {
+        type: 'text',
+        name: 'customerName',
+        title: 'What is your name?',
+        isRequired: true
+      }
+    ]
+  };
+
+  onComplete(survey) {
+    console.log('Survey results: ' + JSON.stringify(survey.data));
+  }
+
+  render() {
+    return (
+      <div>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>eCommerce Category Mappings Comparison</h1>
+        </header>
+
+        <Survey.Survey json={this.json} onComplete={this.onComplete} />
+      </div>
+    );
+  }
 }
 
 export default App;
